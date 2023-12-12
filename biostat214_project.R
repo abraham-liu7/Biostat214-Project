bab53463562411e66e8122d680c64afd11337e50
abraham-liu7 <153321496+abraham-liu7@users.noreply.github.com>
2023-12-12 15:30:28 -0800

    Update biostat214_project.R
    Fixed the radio button - "Full" option doesn't output plots correctly.

--- biostat214_project.R
+++ biostat214_project.R
@@ -5,24 +5,27 @@ library(shiny)
 
 # Define UI for application
 ui <- fluidPage(
-
+  
   # Application title
   titlePanel("2024 US Presidential Election Poll Predictions"),
-
-
+  
+  
   # Adding a main panel with a title and plot output
   mainPanel(
     # Output: Tabset w/ plot, summary, and table ----
     tabsetPanel(type = "tabs",
-                tabPanel("Introduction", value = 1, textOutput("intro1"),textOutput("intro2")),
+                tabPanel("Introduction", value = 1, textOutput("intro1"),
+                         textOutput("intro2")),
                 tabPanel("Plots", value = 2, sidebarPanel(
                   br(),
                   selectInput("pop", "Voter Population:",
-                              c("Adults" = "a", 
+                              c("Full" = "f",
+                                "Adults" = "a", 
                                 "Registered Voters" = "rv",
                                 "Likely Voters" = "lv")),
                   br()),
-                  mainPanel(plotOutput("ybardistPlot")), br(), br(), mainPanel(plotOutput("sigmadistPlot"))),
+                  mainPanel(plotOutput("ybardistPlot")), br(), br(), 
+                  mainPanel(plotOutput("sigmadistPlot"))),
                 id = "conditionedPanels"
     )
   )
@@ -30,7 +33,7 @@ ui <- fluidPage(
 
 # Define server logic
 server <- function(input, output) {
-
+  
   output$intro1 <- renderText({
     return("The US Presidential is the most important election that happens
     every four years. The 2024 Election is no different. Incumbent President
@@ -48,42 +51,61 @@ server <- function(input, output) {
   })
   # Only run once
   Polls <- read.csv("approval_polllist.csv")
-
-  y_bar <- Polls$approve[1:100]/100
-  n <- Polls$sample_size[1:100]
-  sample_var <- (y_bar)*(1-y_bar)/n
-
-  a0 <- 3
-  b0 <- 1
-  theta <- 0.40
-  n0 <- 10
-  N <- 200000000
-  f <- n/N
-  f0 <- n0/N
-  lambda <- n/(1 - f)
-  lambda0 <- n0/(1 + f0)
-  n_survey <- length(y_bar) # T in overleaf
-  y_bar_weighted <- sum(lambda*y_bar)/sum(lambda) # y_bar_dot in overleaf
-
-  astar <- sum(n/2) - n_survey + a0
-  bstar <- b0 + 1/2*((lambda0*(sum(lambda)))/(lambda0 + sum(lambda))*(y_bar_weighted - theta)^2 + sum(lambda*(y_bar_weighted - y_bar)^2) + sum((n-1)*sample_var))
-
-  n_mc <- 10000
-  sigma <- rinvgamma(n_mc, astar, bstar)
-
-  mu_post <- (lambda0*theta + sum(lambda*y_bar))/(lambda0 + sum(lambda))
-  sigma_post <- sigma/(lambda0 + sum(lambda))
-  Y_bar <- rnorm(n_mc, mean = mu_post, sd = sqrt(sigma_post))
-
-
+  
+  posterior_sampler <- function(df = df) {
+    set.seed(0)
+    y_bar <- df$approve[1:100]/100
+    n <- df$sample_size[1:100]
+    sample_var <- (y_bar)*(1-y_bar)/n
+    
+    a0 <- 3
+    b0 <- 1
+    theta <- 0.40
+    n0 <- 10
+    N <- 200000000
+    f <- n/N
+    f0 <- n0/N
+    lambda <- n/(1 - f)
+    lambda0 <- n0/(1 + f0)
+    n_survey <- length(y_bar) # T in overleaf
+    y_bar_weighted <- sum(lambda*y_bar)/sum(lambda) # y_bar_dot in overleaf
+    
+    astar <- sum(n/2) - n_survey + a0
+    bstar <- b0 + 1/2*((lambda0*(sum(lambda)))/(lambda0 + sum(lambda))*
+                         (y_bar_weighted - theta)^2 + 
+                         sum(lambda*(y_bar_weighted - y_bar)^2) + 
+                         sum((n-1)*sample_var))
+    
+    n_mc <- 10000
+    sigma <- rinvgamma(n_mc, astar, bstar)
+    
+    mu_post <- (lambda0*theta + sum(lambda*y_bar))/(lambda0 + sum(lambda))
+    sigma_post <- sigma/(lambda0 + sum(lambda))
+    Y_bar <- rnorm(n_mc, mean = mu_post, sd = sqrt(sigma_post))
+    post_samples <- data.frame(Y_bar, mu_post, sigma_post)
+    return(post_samples)
+  }
+  
+  samples <- eventReactive(input$pop, {
+    if (input$pop == "Full") {
+      posterior_sampler(df = Polls)
+    }
+    else {
+      posterior_sampler(df = Polls[Polls$population == input$pop,])
+    }
+    })
+  
   # You should define all the rendered output in the server
   output$ybardistPlot <- renderPlot({
-  return(ggplot(data = as.data.frame(y_bar), aes(x = y_bar)) + geom_density() +
+    return(ggplot(data = as.data.frame(samples()$Y_bar), aes(x = samples()$Y_bar)) 
+           + geom_density() +
              ggtitle("Posterior Distribution for Y Bar"))
   })
-
+  
   output$sigmadistPlot <- renderPlot({
-    return(ggplot(data = as.data.frame(sigma_post), aes(x = sigma_post)) + geom_density() +
+    return(ggplot(data = as.data.frame(samples()$sigma_post),
+                  aes(x = samples()$sigma_post)) 
+           + geom_density() +
              ggtitle("Posterior Distribution for Sigma"))
   })
 }

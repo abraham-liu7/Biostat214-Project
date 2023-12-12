541e4b972323a3a4f9cbebd65b9c795e7229b0fa
Andrew Chuang <andrewchuang@ucla.edu>
2023-12-12 14:11:39 -0800

    Adding Will's model code to the RShiny app

--- biostat214_project.R
+++ biostat214_project.R
@@ -7,7 +7,7 @@ library(shiny)
 ui <- fluidPage(
 
   # Application title
-  titlePanel("2020 Election Poll Predictions"),
+  titlePanel("2024 US Presidential Election Poll Predictions"),
 
 
   # Adding a main panel with a title and plot output
@@ -15,8 +15,8 @@ ui <- fluidPage(
     # Output: Tabset w/ plot, summary, and table ----
     tabsetPanel(type = "tabs",
                 tabPanel("Introduction", value = 1, textOutput("intro1"),textOutput("intro2")),
-                tabPanel("Plots", value = 2, plotOutput("distPlot"), plotOutput("kmean"),
-                         textOutput("lowest_k")),
+                tabPanel("Plots", value = 2, plotOutput("ybardistPlot"), plotOutput("kmean"),
+                         textOutput("lowest_k"), plotOutput("sigmadistPlot")),
                 id = "conditionedPanels"
     )
   )
@@ -41,13 +41,44 @@ server <- function(input, output) {
            understanding of all the intricacies regarding this event.")
   })
   # Only run once
-  df <- read_csv("approval_polllist.csv")
-  df$study <- as.factor(df$study)
-  df$treat <- as.factor(df$treat)
+  Polls <- read.csv("approval_polllist.csv")
+
+  y_bar <- Polls$approve[1:100]/100
+  n <- Polls$sample_size[1:100]
+  sample_var <- (y_bar)*(1-y_bar)/n
+
+  a0 <- 3
+  b0 <- 1
+  theta <- 0.40
+  n0 <- 10
+  N <- 200000000
+  f <- n/N
+  f0 <- n0/N
+  lambda <- n/(1 - f)
+  lambda0 <- n0/(1 + f0)
+  n_survey <- length(y_bar) # T in overleaf
+  y_bar_weighted <- sum(lambda*y_bar)/sum(lambda) # y_bar_dot in overleaf
+
+  astar <- sum(n/2) - n_survey + a0
+  bstar <- b0 + 1/2*((lambda0*(sum(lambda)))/(lambda0 + sum(lambda))*(y_bar_weighted - theta)^2 + sum(lambda*(y_bar_weighted - y_bar)^2) + sum((n-1)*sample_var))
+
+  n_mc <- 10000
+  sigma <- rinvgamma(n_mc, astar, bstar)
+
+  mu_post <- (lambda0*theta + sum(lambda*y_bar))/(lambda0 + sum(lambda))
+  sigma_post <- sigma/(lambda0 + sum(lambda))
+  Y_bar <- rnorm(n_mc, mean = mu_post, sd = sqrt(sigma_post))
+
 
   # You should define all the rendered output in the server
-  output$distPlot <- renderPlot({
-    return(ggplot(data = df, aes(x = measure , y = age, color = study)) + geom_point())
+  output$ybardistPlot <- renderPlot({
+    return(ggplot(data = as.data.frame(y_bar), aes(x = y_bar)) + geom_density() +
+             ggtitle("Posterior Distribution for Y Bar"))
+  })
+
+  output$sigmadistPlot <- renderPlot({
+    return(ggplot(data = as.data.frame(sigma_post), aes(x = sigma_post)) + geom_density() +
+             ggtitle("Posterior Distribution for Sigma"))
   })
 }
 
